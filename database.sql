f6c8be5738a0482e399795ff10708505f1952f44
siskoweb <sisko4dev@gmail.com>
2025-01-29 12:17:51 +0100

    feat : create sql query

---
+++ database.sql
@@ -0,0 +1,27 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS clickfit_db;
+USE clickfit_db;
+
+-- Create users table
+CREATE TABLE users (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL,
+    `password` VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL,
+    `type` VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL,
+    `active` TINYINT DEFAULT 1,
+    PRIMARY KEY (`ID`)
+);
+
+-- create storef procedure for adding users
+DELIMITER //
+CREATE PROCEDURE `addUser`(
+    IN p_email VARCHAR(255),
+    IN p_password VARCHAR(255),
+    IN p_type VARCHAR(255)
+)
+BEGIN
+    INSERT INTO users (email, password, type, active)
+    VALUES (p_email, p_password, p_type, 1);
+END //
+DELIMITER ;
+
